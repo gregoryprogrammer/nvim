2f48a0ca3bef4adfe828b0578fda658d3613df8b
Grzegorz Gwóźdź <gregoryprogrammer@gmail.com>
2025-10-01 18:33:19 +0200

    require('lspconfig') is deprecated

--- lua/delorean/configs/lsp.lua
+++ lua/delorean/configs/lsp.lua
@@ -1,19 +1,23 @@
-local lspconfig = require("lspconfig")
+-- local lspconfig = require("lspconfig")
 
-lspconfig.clangd.setup{
-    on_attach = function(client, bufnr)
-        client.server_capabilities.signatureHelpProvider = false
-    end,
-}
+--[[
+   [ lspconfig.clangd.setup{
+   [     on_attach = function(client, bufnr)
+   [         client.server_capabilities.signatureHelpProvider = false
+   [     end,
+   [ }
+   ]]
 
-lspconfig.pyright.setup({})
+-- lspconfig.pyright.setup({})
 
-lspconfig.ols.setup {
-    init_options = {
-        checker_args = "-strict-style",
-        enable_semantic_tokens = true,
-        enable_format = true,
-    },
-}
+--[[
+   [ lspconfig.ols.setup {
+   [     init_options = {
+   [         checker_args = "-strict-style",
+   [         enable_semantic_tokens = true,
+   [         enable_format = true,
+   [     },
+   [ }
+   ]]
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
